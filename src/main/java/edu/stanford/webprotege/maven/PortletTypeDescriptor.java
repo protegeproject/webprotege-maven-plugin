550db56be929cc7fb4f5ef54073cbb047c2a4b51
Matthew Horridge <matthew.horridge@stanford.edu>
2016-05-26 16:04:53 -0700

    Added some documentation

--- src/main/java/edu/stanford/webprotege/maven/PortletTypeDescriptor.java
+++ src/main/java/edu/stanford/webprotege/maven/PortletTypeDescriptor.java
@@ -27,6 +27,15 @@ public class PortletTypeDescriptor implements Comparable<PortletTypeDescriptor> 
 
     private final String tooltip;
 
+    /**
+     * Constructs a PortletTypeDescriptor that describes a type of portlet
+     * @param canonicalClassName The portlet class name.  Not {@code null}.
+     * @param simpleName The simple class name of the portlet.  Not {@code null}.
+     * @param packageName The package name of the portlet.  Not {@code null}.
+     * @param id The porlet id. Not {@code null}.
+     * @param title The portlet title.  Not {@code null}.
+     * @param tooltip The tooltip for the portlet.  Not {@code null}.
+     */
     public PortletTypeDescriptor(String canonicalClassName,
                                  String simpleName,
                                  String packageName,
@@ -44,26 +53,50 @@ public class PortletTypeDescriptor implements Comparable<PortletTypeDescriptor> 
         checkArgument(!tooltip.startsWith("\"") && !tooltip.endsWith("\""));
     }
 
+    /**
+     * Gets the simple name of the portlet class.
+     * @return A string representing the simple name.  Not {@code null}.
+     */
     public String getSimpleName() {
         return simpleName;
     }
 
+    /**
+     * Gets the package name of the portlet class.
+     * @return A string representing the portlet class package name.  Not {@code null}.
+     */
     public String getPackageName() {
         return packageName;
     }
 
+    /**
+     * Gets the canonical class name of the portlet.
+     * @return The canonical class name of the portlet.  Not {@code null}.
+     */
     public String getCanonicalClassName() {
         return canonicalClassName;
     }
 
+    /**
+     * Gets the id of the portlet.
+     * @return The portlet id.  Not {@code null}.
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Gets the title of the portlet.
+     * @return The portlet title.  Not {@code null}.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Gets the tooltip of the portlet.
+     * @return The portlet tooltip.  Not {@code null}.
+     */
     public String getTooltip() {
         return tooltip;
     }
@@ -97,6 +130,8 @@ public class PortletTypeDescriptor implements Comparable<PortletTypeDescriptor> 
                 .addValue(id)
                 .addValue(title)
                 .addValue(canonicalClassName)
+                .addValue(packageName)
+                .addValue(simpleName)
                 .toString();
     }
 
